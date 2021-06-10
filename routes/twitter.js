f6c7fa831bdb18da35fa1f223b8a6df75bfd4b6e
MatthiasvsGitHub <matthias@schmettow.de>
2021-06-10 15:17:50 +0200

    added documentation and /me

--- routes/twitter.js
+++ routes/twitter.js
@@ -4,7 +4,16 @@ twitter.use(express.urlencoded({ extended: false }))
 const client = require('../client.js')
 
 twitter.get('/', (req, res) => {
-    res.json('Api is working!')
+    res.json(`Documentation:
+    get /users --> gets all the users
+    get /users/{id} --> gets only the user with that ID
+    get /users/{id}/messages --> gets all the messages and user info from that specific user, ordered by time
+    post /users --> adds a user -> needs a picture(url), name, email and password
+    get /messages --> gets all the messages, ordered by time
+    get /messages/{id} --> only gets the message with that ID
+    post /messages --> adds a message -> needs text, users_id (time is optional, default is current time)
+    get /me --> gets a random user
+    `)
 })
 
 
@@ -19,6 +28,12 @@ twitter.get('/users/:id', (req, res) => {
     .then(data => res.json(data.rows))
 })
 
+twitter.get('/users/:user/messages', (req, res) => {
+    let {user} = req.params
+    client.query(`SELECT text, time, picture, name, email, users.id FROM messages RIGHT JOIN users ON users.id=users_id WHERE users_id=${user} ORDER BY time ASC`)
+    .then(data => res.json(data.rows))
+})
+
 twitter.post('/users', (req, res) => {
     const {picture, name, email, password} = req.body
     client.query(`INSERT INTO users (picture, name, email, password) VALUES('${picture}', '${name}', '${email}', '${password}') RETURNING *`)
@@ -26,7 +41,7 @@ twitter.post('/users', (req, res) => {
 })
 
 twitter.get('/messages', (req, res) => {
-    client.query(`SELECT * FROM messages`)
+    client.query(`SELECT * FROM messages ORDER BY time ASC`)
     .then(data => res.json(data.rows))
 })
 
@@ -42,9 +57,8 @@ twitter.post('/messages', (req, res) => {
     .then(data => res.json(data.rows))
 })
 
-twitter.get('/users/:user/messages', (req, res) => {
-    let {user} = req.params
-    client.query(`SELECT text, time, picture, name, email, users.id FROM messages RIGHT JOIN users ON users.id=users_id WHERE users_id=${user}`)
+twitter.get('/me', (req, res) => {
+    client.query(`SELECT id, picture, name, email FROM users ORDER BY RANDOM() LIMIT 1`)
     .then(data => res.json(data.rows))
 })
 
